0015fff40190bacf867adf6523b09ba6c5208b92
KeithLaiKB <laidelivery@163.com>
2021-04-24 15:24:43 +0800

    update receiver about setCleanStart and setSessionExpiryInterval

--- paho_mqtt_piece-sender-workspace1/com.learn.paho_mqtt_one.sender/src/main/java/com/learn/paho_mqtt_one/sender/minimalexample/TestMain_qos2.java
+++ paho_mqtt_piece-sender-workspace1/com.learn.paho_mqtt_one.sender/src/main/java/com/learn/paho_mqtt_one/sender/minimalexample/TestMain_qos2.java
@@ -1,4 +1,4 @@
-package com.learn.paho_mqtt_one.sender;
+package com.learn.paho_mqtt_one.sender.minimalexample;
 
 import org.eclipse.paho.mqttv5.client.MqttClient;
 import org.eclipse.paho.mqttv5.client.MqttConnectionOptions;
@@ -6,51 +6,44 @@ import org.eclipse.paho.mqttv5.client.persist.MemoryPersistence;
 import org.eclipse.paho.mqttv5.common.MqttException;
 import org.eclipse.paho.mqttv5.common.MqttMessage;
 
-public class TestMain3 {
+public class TestMain_qos2 {
 
 	public static void main(String[] args) {
 
         //String topic        = "MQTT Examples";
         String topic        = "sensors/temperature";
         //String content      = "Message from MqttPublishSample";
-        String content      = "ÄãºÃ";
-        int qos             = 1;
+        //String content      = "ÄãºÃ";
+        String content      = "hi_myfriend";
+        int qos             = 2;
         //String broker       = "tcp://iot.eclipse.org:1883";
         String broker       = "tcp://localhost:1883";
+        //String broker       = "ssl://localhost:8883";
         String clientId     = "JavaSample";
         MemoryPersistence persistence = new MemoryPersistence();
-
+        
+        
+        //QoS0
+        //53144		->	1883	MQTT		Publish Message		hello_nihao
+        //1883		->	53144	TCP			ACK
+        //1883		->	53144	MQTT		Publish Received
+        //53144		->	1883	TCP			ACK
+        //53144		->	1883	MQTT		Publish Release
+        //1883		->	53144	TCP			ACK
+        //1883		->	53144	MQTT		Publish Complete
+        //53144		->	1883	TCP			ACK
+        //
         try {
-        	MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
-
-        	
+            MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
             MqttConnectionOptions connOpts = new MqttConnectionOptions();
-            //connOpts.setCleanStart(false);
-            //connOpts.setSessionExpiryInterval(100L);
-            connOpts.setCleanStart(false);
-
-            
+            connOpts.setCleanStart(true);
             System.out.println("Connecting to broker: "+broker);
             sampleClient.connect(connOpts);
             System.out.println("Connected");
-            // System.out.println("Publishing message: "+content);
-            
-            
-
-            
-            MqttMessage message_tmp;
-            String str_content_tmp;
-            for(int i=0; i<=1000; i++) {
-            	//str_content_tmp = content +":"+(i+1);
-            	str_content_tmp = content +":"+(i+1);
-            	message_tmp = new MqttMessage(str_content_tmp.getBytes());
-            	message_tmp.setQos(qos);
-            	//
-            	System.out.println("Publishing message: "+str_content_tmp);
-                sampleClient.publish(topic, message_tmp);
-                Thread.sleep(10000);
-            }
-            
+            System.out.println("Publishing message: "+content);
+            MqttMessage message = new MqttMessage(content.getBytes());
+            message.setQos(qos);
+            sampleClient.publish(topic, message);
             System.out.println("Message published");
             //
             //sampleClient.disconnect();
@@ -63,10 +56,7 @@ public class TestMain3 {
             System.out.println("cause "+me.getCause());
             System.out.println("excep "+me);
             me.printStackTrace();
-        } catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        }
     }
 
 }
