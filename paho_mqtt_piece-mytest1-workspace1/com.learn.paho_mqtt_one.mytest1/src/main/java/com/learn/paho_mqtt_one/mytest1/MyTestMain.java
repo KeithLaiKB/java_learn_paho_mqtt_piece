caa5070d4677d09cb9bad47879ee133d139a67e7
KeithLaiKB <laidelivery@163.com>
2021-06-05 05:02:02 +0800

    add test

---
+++ paho_mqtt_piece-mytest1-workspace1/com.learn.paho_mqtt_one.mytest1/src/main/java/com/learn/paho_mqtt_one/mytest1/MyTestMain.java
@@ -0,0 +1,10 @@
+package com.learn.paho_mqtt_one.mytest1;
+
+public class MyTestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+	}
+
+}
---
+++ paho_mqtt_piece-mytest1-workspace1/com.learn.paho_mqtt_one.mytest1/src/test/java/com/learn/paho_mqtt_one/mytest1/testest.java
@@ -0,0 +1,5 @@
+package com.learn.paho_mqtt_one.mytest1;
+
+public class testest {
+
+}
